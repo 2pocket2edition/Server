ccdc4e19d085266fa867fa76615b3b1a64d5d35f
DaMatrix <joey.rabil@gmail.com>
2018-03-24 10:22:43 +0100

    split *run* into own submodule

---
+++ run/src/main/java/net/daporkchop/wrapper/WrapperMain.java
@@ -0,0 +1,14 @@
+package net.daporkchop.wrapper;
+
+import cn.nukkit.Nukkit;
+
+/**
+ * lol jef
+ *
+ * @author DaPorkchop_
+ */
+public class WrapperMain {
+    public static void main(String... args) {
+        Nukkit.main(args);
+    }
+}
