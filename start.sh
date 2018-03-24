861e1f26501aa7af26005338b3aaa25f1ab38d5d
DaMatrix <joey.rabil@gmail.com>
2018-03-24 03:24:02 +0100

    and again

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ do
     echo "Press Ctrl+C to stop"
     sleep 3
     git pull
-    mvn compile exec:exec -Dexec.executable="java" -Dexec.args="-classpath %classpath -Xmx6G -Xms6G cn.nukkit.Nukkit"
+    mvn compile --non-recursive exec:exec -Dexec.executable="java" -Dexec.args="-classpath %classpath -Xmx6G cn.nukkit.Nukkit"
 done
